4178e04230f2faa476c108872044bb7b689e61be
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-06-23 11:14:03 +0200

    [200~[FIX] Replace RequestHandler with explicit Request/Response types to fix return type mismatch

--- ABAC/server/src/routes/authRoutes.ts
+++ ABAC/server/src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { register, login } from "../controllers/authController";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Auth route works!");
-});
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
