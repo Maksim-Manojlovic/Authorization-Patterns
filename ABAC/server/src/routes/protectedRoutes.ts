5bc35495b9d35a16032a72bee03e4b07fdd201da
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-06-24 13:53:12 +0200

    [FEATURE] Add ABAC-protected routes for user and moderator roles

--- ABAC/server/src/routes/protectedRoutes.ts
+++ ABAC/server/src/routes/protectedRoutes.ts
@@ -5,12 +5,32 @@ import { verifyToken } from "../middleware/authMiddleware"; // koristi verifyTok
 const router = express.Router();
 
 // Prvo JWT provera, onda ABAC check
+// Admin ruta
 router.get(
   "/admin-only",
   verifyToken, // autentikacija (JWT)
   checkAttribute({ attribute: "role", value: "admin" }), // autorizacija (ABAC)
   (req, res) => {
     res.json({ message: "Welcome, admin!" });
+  }
+);
+//  User ruta
+router.get(
+  "/user-only",
+  verifyToken,
+  checkAttribute({ attribute: "role", value: "user" }),
+  (req, res) => {
+    res.json({ message: "Welcome, user!" });
+  }
+);
+
+//  Moderator ruta
+router.get(
+  "/moderator-only",
+  verifyToken,
+  checkAttribute({ attribute: "role", value: "moderator" }),
+  (req, res) => {
+    res.json({ message: "Welcome, moderator!" });
   }
 );
 
